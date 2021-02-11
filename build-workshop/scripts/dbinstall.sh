2b8585b325ee5b163135a901e7376b41aaf5c4fc
Frey Khademi <frey.khademi@gmail.com>
2021-02-11 19:03:32 +0100

    update

--- build-workshop/scripts/dbinstall.sh
+++ build-workshop/scripts/dbinstall.sh
@@ -4,10 +4,10 @@
 
 sudo apt-get update
 
-apt-get install haproxy -y
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=DE/ST=BW/L=Schwetzingen/O=Aviatrix/CN=dynamodb.eu-central-1.amazonaws.com" -keyout /etc/haproxy/cert.key -out /etc/haproxy/cert.crt
-cat /etc/haproxy/cert.crt /etc/haproxy/cert.key > /etc/haproxy/cert.pem
-echo "frontend ProdWeb
+sudo apt-get install haproxy -y
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=DE/ST=BW/L=Schwetzingen/O=Aviatrix/CN=dynamodb.eu-central-1.amazonaws.com" -keyout /etc/haproxy/cert.key -out /etc/haproxy/cert.crt
+sudo bash -c 'cat /etc/haproxy/cert.crt /etc/haproxy/cert.key > /etc/haproxy/cert.pem'
+sudo bash -c 'echo "frontend ProdWeb
    bind  *:80
     mode http
     stats enable
@@ -27,6 +27,6 @@ frontend ProdWeb-SSL
     use_backend api_gateway
 
 backend api_gateway
-     server api-gateway dynamodb.eu-central-1.amazonaws.com:443 ssl verify none sni str(dynamodb.eu-central-1.amazonaws.com)" >> /etc/haproxy/haproxy.cfg
+     server api-gateway dynamodb.eu-central-1.amazonaws.com:443 ssl verify none sni str(dynamodb.eu-central-1.amazonaws.com)" >> /etc/haproxy/haproxy.cfg'
 
-service haproxy restart
+sudo service haproxy restart
